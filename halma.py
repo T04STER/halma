aa245931dc37ae8d6b29f661f541caae6f3e35da
Dawid Glinkowski <dawid.g.dg@gmail.com>
2024-04-14 16:41:10 +0200

    changes

---
+++ halma.py
@@ -0,0 +1,21 @@
+from dataclasses import dataclass
+from typing import Literal, Union
+
+import numpy as np
+
+
+
+class Halma:
+    """
+        Class representing game      
+    """
+    def __init__(self, board) -> None:
+        self.board = board
+
+    def get_availible_moves(self):
+        pass
+    
+    
+
+if __name__ == '__main__':
+    halma = Halma()
