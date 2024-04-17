cde39918e45645ad5e099f4dc2018a058f9a6feb
Dawid Glinkowski <dawid.g.dg@gmail.com>
2024-04-17 11:51:14 +0200

    add GameState class

--- game_tree.py
+++ game_tree.py
@@ -1,4 +1,5 @@
 
+import cProfile
 from dataclasses import dataclass
 from functools import cache
 from numbers import Number
@@ -6,45 +7,46 @@ import random
 from typing import Tuple
 
 import numpy as np
-from halma import Halma, Move, get_board
+from halma import *
 from utils import timeit
 
 
 @dataclass
 class Node:
     move: Move # last move that led to this postion
-    board: np.ndarray
+    game_state: GameState
 
 class GameTree:
     def __init__(self, halma: Halma) -> None:
-        self.halma: Halma= halma
-        self.start_node = Node(None, self.halma.board)
+        self.halma: Halma = halma
+        self.start_node = Node(None, self.halma.game_state)
 
-    def check_terminal_condition(self, board: np.ndarray):
-        winner = self.halma.check_win_condition(board)
+    def check_terminal_condition(self, game_state: GameState):
+        winner = self.halma.check_win_condition(game_state)
         if winner == 1:
             return float('inf')
         elif winner == 2:
             return float('-inf')
         return 0
 
-    def heurestic(self, board: np.ndarray) -> Number:
+    def heurestic(self, game_state: GameState) -> Number:
         """also checks win/lose condition (+inf, -inf)"""    
-        end_game = self.check_terminal_condition(board)
+        end_game = self.check_terminal_condition(game_state)
         if end_game != 0:
             return end_game
         return random.randint(-100, 100)
     
     def minmax(self, node: Node, depth: int, maximizing_agent: bool, initial_move:Move=None) -> Tuple[int, Move]:
-        heurestic = self.heurestic(node.board)
+        gs = node.game_state
+        heurestic = self.heurestic(gs)
         if depth == 0 or heurestic == float('inf') or heurestic == float('-inf'):
             return heurestic, initial_move
         
         if maximizing_agent:
-            move_list = self.halma.get_available_moves(player=1, board=node.board)
+            move_list = self.halma.get_available_moves(1, gs)
             minmax_generator = (
                 self.minmax(
-                    Node(move, self.halma.make_virtual_move(move, node.board)),
+                    Node(move, self.halma.make_virtual_move(move, gs)),
                     depth-1,
                     False,
                     move if not initial_move else initial_move
@@ -54,10 +56,10 @@ class GameTree:
             
             return max(minmax_generator, key=lambda res:res[0])
         else:
-            move_list = self.halma.get_available_moves(player=2, board=node.board)
+            move_list = self.halma.get_available_moves(player=2, game_state=gs)
             minmax_generator = (
                 self.minmax(
-                    Node(move, self.halma.make_virtual_move(move, node.board)),
+                    Node(move, self.halma.make_virtual_move(move, gs)),
                     depth-1,
                     True,
                     move if not initial_move else initial_move
@@ -67,21 +69,21 @@ class GameTree:
             return min(minmax_generator, key=lambda res:res[0])
     
     def play(self, depth=2, player=True, max_count=10):
-        game_state = 0
+        win_condition = 0
         counter = 0
-        while game_state != float('-inf') and game_state != float('inf') and counter < max_count:
-            game_state, move = self.minmax(
-                Node(None, self.halma.board),
+        while win_condition != float('-inf') and win_condition != float('inf') and counter < max_count:
+            win_condition, move = self.minmax(
+                Node(None, self.halma.game_state),
                 depth,
                 player
-            )
+            )            
             self.halma.make_move(move)
             player = not player
             counter += 1
             
-        return self.halma.board
+        return self.halma.game_state.board
 
 if __name__ == '__main__':
     halma = Halma(get_board())
     gt = GameTree(halma)
-    print(gt.play(max_count=20))
+    cProfile.run('gt.play(depth=3, max_count=5)')
--- halma.py
+++ halma.py
@@ -1,3 +1,4 @@
+from copy import copy
 from dataclasses import dataclass
 from utils import timeit
 from typing import List, Literal, Optional, Set, Tuple, Union
@@ -9,16 +10,52 @@ class Move:
     src: Tuple[int, int]
     dest: Tuple[int, int]
 
+class GameState:
+    def __init__(self, board: np.ndarray, player_1: Set[Tuple[int, int]], player_2: Set[Tuple[int, int]]) -> None:
+        self.board = board
+        self.player_1: Set[Tuple[int, int]] = player_1
+        self.player_2: Set[Tuple[int, int]] = player_2
+    
+    def make_move(self, move: Move):
+        xs, ys = move.src 
+        xd, yd = move.dest
+        player = self.board[xs][ys]
+        self.board[xd, yd] = player
+        if player == 1:
+            self.player_1.remove((xs, ys))
+            self.player_1.add((xd, yd))
+        else:
+            self.player_2.remove((xs, ys))
+            self.player_2.add((xd, yd))
+    
+    def copy(self) -> 'GameState':
+        return GameState(
+            self.board.copy(),
+            copy(self.player_1),
+            copy(self.player_2)
+        )
+
+    def new_state(self, move: Move):
+        gs =  self.copy()
+        gs.make_move(move)
+        return gs
+
+    @classmethod
+    def create_game_state_from_board(cls, board:np.ndarray):
+        player_1 = set(tuple(p) for p in np.dstack(np.where(board==1))[0])
+        player_2 = set(tuple(p) for p in np.dstack(np.where(board==-1))[0])
+        return cls(board, player_1, player_2)
+    
+
 class Halma:
     """
         Class representing game   
     """
     BOARD_SIZE = 16
     def __init__(self, board: np.ndarray) -> None:
-        self.board = board
-        self.player1_camp = set(tuple(p) for p in np.dstack(np.where(board==1))[0])
-        self.player2_camp = set(tuple(p) for p in np.dstack(np.where(board==-1))[0])
-        
+        self.game_state: GameState = GameState.create_game_state_from_board(board)
+        self.player1_camp: Set[Tuple[int, int]] = copy(self.game_state.player_1)
+        self.player2_camp: Set[Tuple[int, int]]= copy(self.game_state.player_2)
         
     def _is_in_board(self, x, y):
         return (0 <= x < self.BOARD_SIZE) and (0 <= y < self.BOARD_SIZE)
@@ -41,6 +78,7 @@ class Halma:
         return 2*over[0] - src[0], 2*over[1] - src[1]
 
     def get_pawn_moves(self, position, board) -> List[Move]:
+        # TODO: add not outside camp lock
         x, y = position
         moves_tup = []
         for i in range(max(0, x - 1), min(self.BOARD_SIZE, x+2)):
@@ -58,40 +96,33 @@ class Halma:
         player_pawn = player if player == 1 else -1
         return np.dstack(np.where(board==player_pawn))[0]
         
-    def get_available_moves(self, player: Literal['1', '2'], board: np.ndarray=None) -> List[Move]:
+    def get_available_moves(self, player: Literal['1', '2'], game_state:Optional[GameState]=None) -> List[Move]:
         moves = []
-        if board is None:
-            board = self.board
-        pawn_list = self.get_pawn_list(player, board)
+        if game_state is None:
+            game_state = self.game_state
+        pawn_list = game_state.player_1 if player == 1 else game_state.player_2
 
         for pos in pawn_list:
-            moves.extend(self.get_pawn_moves(pos, board))
+            moves.extend(self.get_pawn_moves(pos, game_state.board))
+            
         return  moves
 
     def make_move(self, move: Move):
-        xs, ys = move.src 
-        xd, yd = move.dest
-        self.board[xd][yd] = self.board[xs][ys]
-        self.board[xs][ys] = 0
+        self.game_state.make_move(move)
 
-    def make_virtual_move(self, move, board):
-        "returns a copy of board with made move"
-        xs, ys = move.src 
-        xd, yd = move.dest
-        board = board.copy()
-        board[xd][yd] = board[xs][ys]
-        board[xs][ys] = 0
-        return board
+    def make_virtual_move(self, move: Move, game_state: GameState) -> GameState:
+        "returns a copy of gamestate after move"
+        return game_state.new_state(move)
+
     
-    def check_win_condition(self, board=None) -> Literal['0', '1', '2']:
-        board = board if board is None else self.board
-        
-        player_1 = set(tuple(p) for p in self.get_pawn_list(1, board))
-        if player_1 == self.player2_camp:
+    def check_win_condition(self, game_state:Optional[GameState]=None) -> Literal['0', '1', '2']:
+        if not game_state:
+            game_state = self.game_state
+
+        if game_state.player_1 == self.player2_camp:
             return 1
         
-        player_2 = set(tuple(p) for p in self.get_pawn_list(2, board))
-        if player_2 == self.player1_camp:
+        if game_state.player_2 == self.player1_camp:
             return 2
         
         return 0
@@ -137,4 +168,4 @@ def get_board():
 
 if __name__ == '__main__':
     halma = Halma(get_board())
-    print(pawns:=halma.get_pawn_list(1, halma.board))
+    print(pawns:=halma.get_pawn_list(1, halma.game_state.board))
